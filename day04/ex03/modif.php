f0077f14327daecb4d5774edb3c6a6c4486dc775
William-griffin SABATES <wsabates@e2r7p12.42.fr>
2018-05-13 22:56:23 +0200

    update

---
+++ day04/ex03/modif.php
@@ -0,0 +1,32 @@
+<?php
+if ($_POST["submit"] == "OK" && $_POST["newpw"] != NULL && $_POST["login"] != NULL)
+{
+	$oldpw = hash(sha512, $_POST["oldpw"]);
+	$newpw = hash(sha512, $_POST["newpw"]);
+	$content = unserialize(file_get_contents("../private/passwd"));
+	$i = 0;
+	foreach($content as $elem)
+	{
+		if ($elem["login"] == $_POST["login"] && $elem["passwd"] == $oldpw)
+		{
+			$content[$i]["passwd"] = $newpw;
+			$changed = 1;
+		}
+		if ($elem["passwd"] != $oldpw)
+		{
+			echo "ERROR\n";
+		}
+		$i++;
+	}
+	if ($changed == 1)
+	{
+		$serialised = serialize($content);
+		file_put_contents("../private/passwd", $serialised);
+		echo "OK\n";
+	}
+}
+else
+{
+	echo "ERROR\n";
+}
+?>
